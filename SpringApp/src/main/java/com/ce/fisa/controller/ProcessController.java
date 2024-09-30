2f53e1ea3dd01655eca9db772dddc06f56fc3724
Hyeonbeen Lee <147581930+Been980804@users.noreply.github.com>
2024-09-30 15:39:23 +0900

    Update ProcessController.java

--- SpringApp/src/main/java/com/ce/fisa/controller/ProcessController.java
+++ SpringApp/src/main/java/com/ce/fisa/controller/ProcessController.java
@@ -11,7 +11,7 @@ public class ProcessController {
 	
 	@GetMapping("/test")
 	public String reqRes() {
-		log.info("요청 수락 했습니다~~!!!");
+		log.info("요청 수락 했습니다~~!!!ㅎㅎㅎ");
 		return "linux 서버에서 실행되는 app";
 	}
 }
